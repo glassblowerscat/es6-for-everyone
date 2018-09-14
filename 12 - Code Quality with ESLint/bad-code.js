a5f4ef027f895f68da10363fe615ce42d99651b3
glassblowerscat <glassblower.random@gmail.com>
2018-09-14 08:05:18 -0400

    Line and File Specific Settings and ESLint Plugins Completed

--- 12 - Code Quality with ESLint/bad-code.js
+++ 12 - Code Quality with ESLint/bad-code.js
@@ -1,3 +1,5 @@
+/* globals twttr ga */
+
 const weather = new Promise((resolve) => {
   setTimeout(() => {
     resolve({ temp: 29, conditions: 'Sunny with Clouds' });
@@ -29,3 +31,34 @@ Promise
 
 ga.track();
 twttr.trackConversion();
+
+/* eslint-disable */
+if (!Array.prototype.includes) {
+  Array.prototype.includes = function (searchElement /*, fromIndex*/) {
+    'use strict';
+    var O = Object(this);
+    var len = parseInt(O.length, 10) || 0;
+    if (len === 0) {
+      return false;
+    }
+    var n = parseInt(arguments[1], 10) || 0;
+    var k;
+    if (n >= 0) {
+      k = n;
+    } else {
+      k = len + n;
+      if (k < 0) { k = 0; }
+    }
+    var currentElement;
+    while (k < len) {
+      currentElement = O[k];
+      if (searchElement === currentElement ||
+        (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+        return true;
+      }
+      k++;
+    }
+    return false;
+  };
+}
+/* eslint-enable */
